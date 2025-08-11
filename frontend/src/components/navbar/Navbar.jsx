7d54b72de93e60a4f96ac68c0afc33515bb773e5
Melanie Kent <mskent@live.ca>
2025-08-10 17:35:07 -0700

    navbar rough draft with logo, no mobile responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import "./Navbar.css";
 
 export const Navbar = () => {
+  const [selectedLink, setSelectedLink] = useState("");
+
   return (
-    <div>
-      Navbar
+    <div className="navbar-container">
+      <div className="navbar-inner-container">
+        <img
+          className="navbar-logo"
+          src="/maddys_logo.png"
+          alt="Maddy's Cream Bike Logo"
+        />
+        <div className="navbar-links-container">
+          <a className="navbar-link">
+            HOME
+          </a>
+          <a className="navbar-link">
+            MENU & CATERING
+          </a>
+          <a className="navbar-link">
+            WHY MADDY'S
+          </a>
+          <a className="navbar-link">
+            OUR STORY
+          </a>
+          <a className="navbar-link">
+            FIND US
+          </a>
+          <div className="navbar-icons-container">
+            <FontAwesomeIcon className="navbar-icon" icon={faFacebookF} size="lg" />
+            <FontAwesomeIcon className="navbar-icon" icon={faInstagram} size="lg" />
+            <FontAwesomeIcon className="navbar-icon" icon={faEnvelope} size="lg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -2,8 +2,22 @@ import "./HomePage.css"
 
 export const HomePage = () => {
   return (
-    <div>
-      Home Page
+    <div className="homepage-container">
+      <div>
+        HOME
+      </div>
+      <div>
+        MENU & CATERING
+      </div>
+      <div>
+        WHY MADDY'S
+      </div>
+      <div>
+        OUR STORY
+      </div>
+      <div>
+        FIND US
+      </div>
     </div>
   );
 }
