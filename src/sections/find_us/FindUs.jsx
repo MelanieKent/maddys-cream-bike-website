f0face454ebf1d4e6fccac7585ef37e839a3f35b
Melanie Kent <mskent@live.ca>
2025-10-04 11:11:54 -0700

    resolve intersection observer bug

--- src/sections/find_us/FindUs.jsx
+++ src/sections/find_us/FindUs.jsx
@@ -8,6 +8,38 @@ export const FindUs = () => {
           <p className="find-us-title">Find Us</p>
         </div>
         <p className="previous-events-title">Previous Events</p>
+        {/* Inspired by: https://github.com/Girls-in-Steam/girls-in-steam-official/blob/main/src/pages/components/TheTeam.js */}
+        <div className="previous-events-slider">
+        <div className="events-image-slide">
+          <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                    <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                              <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                        <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                  <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                            <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                      <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                                <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                                          <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                                                    <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                    <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                              <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                        <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                  <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                            <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                      <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                                <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+                                                                                          <img src={`${process.env.PUBLIC_URL}/placeholder_cookie_1.png`} />
+          {/* {bioData.map((bio) => (
+            <img
+              src={bio.src}
+              alt={bio.name}
+              key={bio.name}
+              onClick={() => handleBioImgClick(bio)}
+            />
+          ))} */}
+        </div>
+
+      </div>
       </div>
       <div className="footer">
         <p className="footer-copyright-text">
--- src/sections/our_story/OurStory.jsx
+++ src/sections/our_story/OurStory.jsx
@@ -4,7 +4,7 @@ export const OurStory = () => {
   return (
     <div className="our-story-container">
       <p className="our-story-title">Where It All Began</p>
-      <img className="our-story-visual-spacer" src="placeholder_1.png" alt="Placeholder 1" />
+      <img className="our-story-visual-spacer" src={`${process.env.PUBLIC_URL}/placeholder_1.png`} alt="Placeholder 1" />
       <p className="our-story-title">Testimonials</p>
       <p className="testimonials-subheader">See What Our Customers Say:</p>
     </div>
